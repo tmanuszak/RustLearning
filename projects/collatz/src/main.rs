336b78cda08d22b1ad6ba0580f765bbc463ad86d
Trey Manuszak <manuszaktrey@gmail.com>
2023-02-20 15:58:29 -0700

    Added some efficiency improvements and used functions.

--- projects/collatz/src/main.rs
+++ projects/collatz/src/main.rs
@@ -1,5 +1,5 @@
 /// Find the first collatz number with a path length of input.
-/// Print -1 if it doesnt exist.
+/// Print 0 if it doesnt exist.
 use std::io;
 use std::cmp::Ordering;
 use std::collections::{HashMap, VecDeque};
@@ -14,66 +14,102 @@ fn main() {
             .read_line(&mut input)
             .expect("Failed to read line.");
 
-        let q = String::from("q");
+        // If no input, continue loop
+        if input.trim().len() == 0 {
+            continue;
+        }
 
+        // See if we quit
+        let q = String::from("q");
         match input.trim().cmp(&q) {
             Ordering::Equal => break 'get_input,
             _ => (),
         }
 
+        // Parse input into u32
         let input: u32 = input.trim().parse().expect("Please type a number!");
 
-        let mut start: u128 = 2;
-        let mut length_found = false;
-        let mut smallest: u128 = u128::MAX;
+		println!("{}", collatz(input));
+	
+	}
+}
 
-        let mut start_length = HashMap::new();
-        start_length.insert(1, 1);
+fn collatz(input: u32) -> u128 {
+	if input < 2 {
+		return input.into();
+	}
 
-        if input == 1 {
-            length_found = true;
-        }
+	// Starting to find the result
+	// Variables
+	let mut start: u128 = 2; // start of the path
+	let mut length_found = false; // if we have found the length
+	let mut smallest: u128 = 2u128.saturating_pow(input); // the smallest the answer could be
 
-        // While we havent found the answer
-        while !length_found || start < smallest {
-            // if the start path length hasnt been found
-            if !start_length.contains_key(&start) {
-                let mut d = VecDeque::new();
-                d.push_back(start);
-                let mut next: u128 = if start % 2 == 0 {
-                    start / 2
-                } else {
-                    3 * start + 1
-                };
+	// Hash map where start -> length
+	let mut start_length = HashMap::new();
+	start_length.insert(1u128, 1u32);
 
-                // while we dont know the path length of the next value
-                while !start_length.contains_key(&next) {
-                    d.push_back(next);
-                    next = if next % 2 == 0 {
-                        next / 2
-                    } else {
-                        3 * next + 1
-                    };
-                }
+	// Stores the path of the current working iteration
+	let mut d = VecDeque::new();
+	let mut len: u32 = 0;  // length of the queue, since .len() -> usize
 
-                let mut length = match start_length.get(&next) {
-					Some(value) => *value,
-					None => 0,
-				}; 
-				for value in d.iter().rev() {
-                    length += 1;
-                    if length == input {
-                        length_found = true;
-                        if *value < smallest {
-                            smallest = *value;
-                        }
-                    }
-                    start_length.insert(*value, length);
-                }
-            }
-            start += 1;
-        }
+	// While we havent found the answer
+	while start < smallest {
+		// if the start path length hasnt been found
+		if !start_length.contains_key(&start) {
+			d.clear();
+			d.push_back(start);
+			len = 1;
+			let mut next: u128 = if start % 2 == 0 {
+				start / 2
+			} else {
+				let (mul, of1) = start.overflowing_mul(3);
+				let (res, of2) = mul.overflowing_add(1);
+				if of1 || of2 {
+					println!("Overflowing operation with start: {start}");
+					return 0;
+				}
+				res
+			};
 
-        println!("{smallest}");
-    }
+			// while we dont know the path length of the next value
+			while !start_length.contains_key(&next) {
+				d.push_back(next);
+				len += 1;
+				next = if next % 2 == 0 {
+					next / 2
+				} else {
+					let (mul, of1) = next.overflowing_mul(3);
+					let (res, of2) = mul.overflowing_add(1);
+					if of1 || of2 {
+						println!("Overflowing operation with start: {start}");
+						return 0;
+					}
+					res 
+				};
+			}
+
+			len = match start_length.get(&next) {
+				Some(value) => *value + len,
+				None => 0, // Shouldnt ever be the case since we broke above
+			};
+			for value in d.iter() {
+				if len == input {
+					length_found = true;
+					if *value < smallest {
+						smallest = *value;
+					}
+				}
+				start_length.insert(*value, len);
+				len -= 1;
+			}
+		}
+		start += 1;
+	}
+
+	if !length_found {
+		return 0
+	} else {
+		return smallest
+	}
 }
