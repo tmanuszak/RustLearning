005683af264e751e530ed3e86a38dc4522e7e0f5
Trey Manuszak <manuszaktrey@gmail.com>
2023-03-03 16:13:35 -0700

    packages, crates, and modules

---
+++ projects/restaurant/src/lib.rs
@@ -0,0 +1,64 @@
+mod front_of_house {
+	pub mod hosting {
+		pub fn add_to_waitlist() {}
+		fn seat_at_table() {}
+	}
+
+	mod serving {
+		fn take_order() {}
+		fn serve_order() {}
+		fn take_payment() {}
+	}
+}
+
+mod back_of_house {
+	pub struct Breakfast {
+		pub toast: String,
+		seasonal_fruit: String,
+	}
+
+	impl Breakfast {
+		pub fn summer(toast: &str) -> Breakfast {
+			Breakfast {
+				toast: String::from(toast),
+				seasonal_fruit: String::from("peaches"),
+			}
+		}
+	}
+
+	// You should always make enums pub. They arent really useful
+	// otherwise.
+	pub enum Appetizer {
+		Soup,
+		Salad,
+	}
+
+	fn fix_incorrect_order() {
+		cook_order();
+		super::deliver_order();
+	}
+
+	fn cook_order() {}
+}
+
+pub fn eat_at_restaurant() {
+	// Absolute path
+	crate::front_of_house::hosting::add_to_waitlist();
+
+	// Relative path
+	front_of_house::hosting::add_to_waitlist();
+
+	// Order a breakfast in the summer with Rye toast
+	let mut meal = back_of_house::Breakfast::summer("Rye");
+	meal.toast = String::from("Wheat");
+	println!("I'd like {} toast please!", meal.toast);
+
+	// The next line will not compile if uncommented; we are not allowed
+	// to see or modify the seasonal fruit that comes with the meal
+	// meal.seasonal_fruit = String::from("blueberries");
+
+	let order1 = back_of_house::Appetizer::Soup;
+	let order1 = back_of_house::Appetizer::Salad;
+}
+
+fn deliver_order() {}
