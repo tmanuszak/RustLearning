3e5d71d81418a68cc7172356491fb991b58aa40d
Trey Manuszak <manuszaktrey@gmail.com>
2023-03-06 15:50:38 -0700

    separating modules

--- projects/restaurant/src/lib.rs
+++ projects/restaurant/src/lib.rs
@@ -1,55 +1,19 @@
-mod front_of_house {
-	pub mod hosting {
-		pub fn add_to_waitlist() {}
-		fn seat_at_table() {}
-	}
+mod front_of_house;
+mod back_of_house;
 
-	mod serving {
-		fn take_order() {}
-		fn serve_order() {}
-		fn take_payment() {}
-	}
-}
+// Absolute path
+pub use crate::front_of_house::hosting;
+pub use crate::back_of_house::{Appetizer, Breakfast};
 
-mod back_of_house {
-	pub struct Breakfast {
-		pub toast: String,
-		seasonal_fruit: String,
-	}
-
-	impl Breakfast {
-		pub fn summer(toast: &str) -> Breakfast {
-			Breakfast {
-				toast: String::from(toast),
-				seasonal_fruit: String::from("peaches"),
-			}
-		}
-	}
-
-	// You should always make enums pub. They arent really useful
-	// otherwise.
-	pub enum Appetizer {
-		Soup,
-		Salad,
-	}
-
-	fn fix_incorrect_order() {
-		cook_order();
-		super::deliver_order();
-	}
-
-	fn cook_order() {}
-}
+// Relative path
+// pub use front_of_house::hosting;
+// pub use back_of_house::{Appetizer, Breakfast};
 
 pub fn eat_at_restaurant() {
-	// Absolute path
-	crate::front_of_house::hosting::add_to_waitlist();
-
-	// Relative path
-	front_of_house::hosting::add_to_waitlist();
+	hosting::add_to_waitlist();
 
 	// Order a breakfast in the summer with Rye toast
-	let mut meal = back_of_house::Breakfast::summer("Rye");
+	let mut meal = Breakfast::summer("Rye");
 	meal.toast = String::from("Wheat");
 	println!("I'd like {} toast please!", meal.toast);
 
@@ -57,8 +21,8 @@ pub fn eat_at_restaurant() {
 	// to see or modify the seasonal fruit that comes with the meal
 	// meal.seasonal_fruit = String::from("blueberries");
 
-	let order1 = back_of_house::Appetizer::Soup;
-	let order1 = back_of_house::Appetizer::Salad;
+	let order1 = Appetizer::Soup;
+	let order1 = Appetizer::Salad;
 }
 
 fn deliver_order() {}
