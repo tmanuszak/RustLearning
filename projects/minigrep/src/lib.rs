91fa222bab6a19006ead1e6ed65b018632af60b5
Trey Manuszak <manuszaktrey@gmail.com>
2023-04-02 10:53:24 -0700

    minigrep Ch 12 done

--- projects/minigrep/src/lib.rs
+++ projects/minigrep/src/lib.rs
@@ -1,9 +1,10 @@
 use std::error::Error;
-use std::fs;
+use std::{fs, env};
 
 pub struct Config {
 	pub query: String,
 	pub file_path: String,
+	pub ignore_case: bool,
 }
 
 impl Config {
@@ -19,16 +20,101 @@ impl Config {
 		let query = args[1].clone(); 
 		let file_path = args[2].clone();
 
-		Ok(Config { query, file_path })
+		// "var" returns Ok() with contents of the value of the environment 
+		// variable, otherwise it will return Err() if the environment variable
+		// does not exist. "is_ok" returns true if Ok() and returns false if 
+		// Err().
+		// EXAMPLE: $  IGNORE_CASE=1 cargo run -- to poem.txt
+		let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+		Ok(Config { 
+			query, 
+			file_path,
+			ignore_case,
+		})
 	}
 }
 
 // Returning nothing, or any struct that implements "Error" trait
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-	// "?" either unwraps the contents if Ok, or returns the error to the caller.
 	let contents = fs::read_to_string(config.file_path)?;
 
-	println!("With text:\n{contents}");
+	let results = if config.ignore_case {
+		search_case_insensitive(&config.query, &contents)
+	} else {
+		search(&config.query, &contents)
+	};
+
+	for line in results {
+		println!("{line}");
+	}
 
 	Ok(())
+}
+
+// We use lifetimes so that the compiler knows which string slice the returned
+// value will be referencing.
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+	let mut results = Vec::new();
+
+	for line in contents.lines() {
+		if line.contains(query) {
+			results.push(line);
+		}
+	}
+
+	results
+}
+
+pub fn search_case_insensitive<'a>(
+	query: &str, 
+	contents: &'a str
+) -> Vec<&'a str> {
+
+	// query is shadowed variable. It will be of type String, not str
+	let query = query.to_lowercase();
+	let mut results = Vec::new();
+
+	for line in contents.lines() {
+		// Need to add '&' because contains takes str types, not String
+		if line.to_lowercase().contains(&query) {
+			results.push(line);
+		}
+	}
+
+	results
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn case_sensitive() {
+		let query = "duct";
+		// The "\" tells Rust not to put a newline at the beginning of the
+		// string literal.
+		let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Duct tape.";
+
+		assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+	}
+
+	#[test]
+	fn case_insensitive() {
+		let query = "rUsT";
+		let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+		
+		assert_eq!(
+			vec!["Rust:", "Trust me."],
+			search_case_insensitive(query, contents)
+		);
+	}	
 }
--- projects/minigrep/src/main.rs
+++ projects/minigrep/src/main.rs
@@ -8,18 +8,15 @@ fn main() {
 	// contents inside the Err() case and use it as input (err) to the anonymous 
 	// function.	
 	let config = Config::build(&args).unwrap_or_else(|err| {
-		println!("Problem parsing arguments: {err}");
+		eprintln!("Problem parsing arguments: {err}");
 		process::exit(1);
 	});
-
-	println!("Searching for {}", config.query);
-	println!("In file {}", config.file_path);
 
 	// If an error is returned from run, handle it.
 	// We use "if let" instead of "unwrap_or_else" because we dont care about
 	// unwrapping the Ok(()) case, we only want to handle an error.
 	if let Err(e) = minigrep::run(config) {
-		println!("Application error: {e}");
+		eprintln!("Application error: {e}");
 		process::exit(1);
 	}
 }
